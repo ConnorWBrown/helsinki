95e01872d656a1e82b80a0c99d0e6d3f03dc4c35
ConnorWBrown <connorwilliambrown@gmail.com>
2025-01-01 15:50:14 -0800

    Objects cannot be returned as children

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -15,20 +15,17 @@ const Hello = (props) => {
     </div>
   )
 }
-const name = 'dr'
-const age = 14
 
 const App = () => {
-
-  const name = 'Peter'
-  const age = 10
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name='Maya' age={26 + 10} />
-      <Hello name={name} age={age} />
+      <p>{friends[0].name} {friends[0].age}</p>
+      {/* <p>{friends[1]}</p> */}
     </div>
   )
 }
