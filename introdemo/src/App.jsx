b480707114697d45812527754cb2daa9885eeefb
ConnorWBrown <connorwilliambrown@gmail.com>
2025-01-01 14:51:27 -0800

    Props overview

--- introdemo/src/App.jsx
+++ introdemo/src/App.jsx
@@ -3,25 +3,36 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const Hello = () => {
+const Hello = (props) => {
+
+  console.log(props)
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+const name = 'dr'
+const age = 14
+
+const App = () => {
+
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
 }
 
-const App = () => {
-  return (
-    <div>
-      <h1>Greetings</h1>
-      
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
-  )
-}
 export default App
 
 // function App() {
