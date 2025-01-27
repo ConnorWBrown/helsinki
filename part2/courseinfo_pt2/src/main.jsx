b1a24bf0f989fc5fbbe9a6c4031fb3323c6e4dbe
ConnorWBrown <connorwilliambrown@gmail.com>
2025-01-27 12:52:20 -0800

    pt2.1 base

--- part2/courseinfo_pt2/src/main.jsx
+++ part2/courseinfo_pt2/src/main.jsx
@@ -1,7 +1,8 @@
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
